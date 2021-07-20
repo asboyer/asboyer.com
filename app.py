9ee344b286c035da52788407c7ce0ae6c57e6124
boyer <asboyer@gmail.com>
2021-07-20 14:46:34 -0400

    changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -20,9 +21,13 @@ def news():
 def blog():
     return render_template("blog/blog.html")
 
-@app.route("/blog/0")
-def blog_0():
-    return render_template("blog/goatgrade.html")
+@app.route("/blog/<name>")
+def blog_post(name):
+    if os.path.exists(f'templates/blog/{name}.html'):
+        return render_template(f"blog/{name}.html")
+    else:
+        # make error page
+        return render_template("blog/blog.html")
 
 # favorites
 
