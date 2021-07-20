bc055e187066fc048375993ea7d0c0bb48e6b00e
Andrew Boyer <asboyer@gmail.com>
2021-07-20 02:11:01 -0400

    soon page

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, send_from_directory
 
 
+
 app = Flask(__name__)
+
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/blog/goatgrade")
+def blog():
+    return render_template("blog/goatgrade.html")
