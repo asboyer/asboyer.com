4f8caab33cc6498e053af179f9de47b1e1aa3521
Andrew Boyer <asboyer@gmail.com>
2021-07-20 02:27:12 -0400

    restructure 2 + news

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, send_from_directory
 
-
-
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/soon")
+def soon():
+    return render_template("soon.html")
+
+@app.route("/press")
+def news():
+    return render_template("about/news.html")
+
+# blogs
 @app.route("/blog/goatgrade")
 def blog():
     return render_template("blog/goatgrade.html")
