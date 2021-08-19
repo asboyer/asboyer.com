b8ff9fa237a4bd3fac3fe76811f57de324db948e
Andrew Boyer <asboyer@gmail.com>
2021-08-18 22:41:15 -0400

    altering music backend script for optimization and current music data, need to test later w/ internet connection

--- music_backend.py
+++ music_backend.py
@@ -15,19 +15,25 @@ bad_data = ['available_markets', 'album_type', 'album_group', 'type',
             'label', 'release_date_precision', 'href', 'genres']
 
 # TO DO:
-# need to figure out how to do top current albums, and top tracks
-# add top tracks field
-# need to also write backend for top songs
+# need to test top albums, top tracks
+# need to write js for top albums, top tracks
+    # figure out how to loop though the list of top tracks in jquery
+# loop through albums and find top tracks
+    # maybe pass top tracks as string arg in original text file
+
+
+############################################################################
 
 def read_music_data_from_file(spec):
-    with open(f'data/add_uris_music_{spec}.txt', 'r') as f:
+    with open(f'add/add_uris_music_{spec}.txt', 'r') as f:
         file_albums = f.readlines()
         albums = []
         for album in file_albums:
-            album = album.strip('\n')
-            new_album = album.split(" :: ")
+            new_album = album.strip('\n')
             albums.append(new_album)
-        return albums
+    f = open(f'add/add_uris_music_{spec}.txt', 'w+')
+    f.close()
+    return albums
 
 def extract_uri(albums):
     uri_list = []
@@ -79,17 +85,67 @@ def update_database(spec):
     with open(f'data/music_{spec}.json', 'w') as json_file: 
         json.dump(data, json_file, indent=4)
 
+def clean_song(result):
+    # need to figure out what sp.track actually returns
+    for data_key in list(result):
+        if data_key in bad_data:
+            del result[data_key]
+    artist_list = []
+    for artist in result['artists']:
+        artist_list.append(artist['name'])
+    artists_string = ''
+    for i in range(len(artist_list)):
+        if i == len(artist_list) - 1:
+            artists_string += artist_list[i]
+        else:
+            artists_string += f'{artist_list[i]}, '
+    result['artists'] = artists_string
+    best_image_url = ''
+    for image in result['images']:
+        if image['height'] == 640:
+            best_image_url = image['url']
+    del result['images']
+    result['image'] = best_image_url
+
+    return result
+
+def build_songs_database(uri_list):
+    final = {}
+    for uri in uri_list:
+        print(uri)
+        song_data = clean_song(sp.track(uri))
+        final[song_data['name']] = song_data
+    return final
+
+def update_songs_database():
+    with open(f'data/music_current_songs.json', 'r') as json_file:
+        data = json.load(json_file)
+
+    data.update(build_songs_database(
+                extract_uri(
+                read_music_data_from_file('current_songs')
+                )
+                )
+                )
+
+    with open(f'data/music_current_songs.json', 'w') as json_file: 
+        json.dump(data, json_file, indent=4)
+
+############################################################################
+
+# misc function
 def data_base_clean():
     with open('data/music_all_time.json', 'r') as json_file:
         data = json.load(json_file)
 
     # data operation
     for album in data:
-        data[album]['top_tracks'] = []
-        del data[album]['top_traks']
+        del data[album]['genres']
 
     with open('data/music_all_time.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
+if __name__ == "__main__":
+    data_base_clean()
 # update_database('all_time')
-new_update()
+# new_update()
