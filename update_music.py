7464fbde126206d9c59b0f53067883c08dc83f72
Andrew Boyer <asboyer@gmail.com>
2021-08-18 22:40:15 -0400

    updating music script

---
+++ update_music.py
@@ -0,0 +1,26 @@
+import sys
+import music_backend
+
+def update_music(spec):
+    music_backend.update_database(spec)
+
+def update_tracks():
+    music_backend.update_songs_database()
+
+if __name__ == "__main__":
+    try:
+        spec = sys.argv[1]
+    except IndexError:
+        specs = ['all_time', 'current']
+
+    if type(spec) == str:
+        if spec == 'songs' or spec == 'tracks':
+            music.backend.update_songs_database()
+        else:
+            update_music(spec)
+    elif type(spec) == list:
+        for spec in specs:
+            update_music(spec)
+        music_backend.update_songs_database()
+    else:
+        print('invalid arg')
