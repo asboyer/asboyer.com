73a059797d89bd9345b7ab72b566f57b8b665432
boyer <asboyer@gmail.com>
2021-08-22 22:19:11 -0400

    testing done -- backup improved

--- backup_and_update.py
+++ backup_and_update.py
@@ -15,6 +15,7 @@ need to write landing page for film, with shows and movies
 import json
 import os
 import sys
+import calendar
 from update_movies import update_movies
 from update_music import update_music, update_tracks
 from update_shows import update_shows
@@ -26,29 +27,46 @@ data_names = ['music_current', 'music_current_songs']
 
 today = datetime.today()
 date_string = f'{today.month}{today.day}{today.year}'
+date_string_string = f'{today.month}/{today.day}/{today.year}'
 
 def backup_current_music_data():
 
     unique_dbs = 2
 
-    f = open('data_backup/last.txt', 'r')
+    f = open('data/archive/last.txt', 'r')
     last_date = f.read()
+    f.close()
+
+    f = open('data/archive/last_date.txt', 'r')
+    last_date_string = f.read()
+    f.close()
+
+    if last_date_string == '':
+        last_date_string = date_string_string
+
+    last_date_list = last_date_string.split('/')
 
     for data_name in data_names:
         # loading data to be backed up into var
-        print(f'# [loading data from {data_name}.json]')
         current_file = open(f'data/{data_name}.json', 'r')
         data = json.load(current_file)
         current_file.close()
         if last_date != '':
-            print()
-            current_file = open(f'data_backup/{data_name}_{last_date}.json')
+            current_file = open(f'data/archive/{last_date}/{data_name}.json')
             old_data = json.load(current_file)
             if data == old_data:
                 unique_dbs -= 1
             if unique_dbs == 0:
-                print('no unique data to backup')
+                print('# [no unique data to backup]')
                 return 0
+
+    comment_head = f"""
+#####################{date_string_string} music backup here#####################
+"""
+    app =  open('app.py', 'a')
+    app.write(comment_head)
+    
+    os.mkdir(f'data/archive/{date_string}', 0o666)
 
     for data_name in data_names:
         print(f'# [loading data from {data_name}.json]')
@@ -56,19 +74,19 @@ def backup_current_music_data():
         data = json.load(current_file)
         current_file.close()
         # writing file name
-        file_name = f'{data_name}_{ate_string}'
+        file_name = data_name
 
         # writing to backup file
-        print(f'# [loading data to {file_name}.json]')
+        print(f'# [loading data to {file_name}.json backup]')
 
-        with open(f'data_backup/{file_name}.json', 'w') as backup_file:
+        with open(f'data/archive/{date_string}/{file_name}.json', 'w') as backup_file:
             json.dump(data, backup_file, indent=4)
 
         # creating display data function
         display_data_function = f"""
-@app.route("/data/archive/{file_name}.json")
+@app.route("/data/archive/{date_string}/{file_name}.json")
 def load_{file_name}():
-    f = open('data_backup/{file_name}.json')
+    f = open('data/archive/{date_string}/{file_name}.json')
     data = json.load(f)
     return data
         """
@@ -78,14 +96,24 @@ def load_{file_name}():
         app =  open('app.py', 'a')
         app.write(display_data_function)
 
+    if last_date_list[0] == today.month:
+        date_range_string = f'{calendar.month_name[today.month]} {today.year}'
+    elif int(last_date_list[2]) != today.year:
+        date_range_string = f'{calendar.month_name[int(last_date_list[0])]} {last_date_list[2]} - {calendar.month_name[today.month]} {today.year}'
+    else:
+        date_range_string = f'{calendar.month_name[int(last_date_list[0])]} - {calendar.month_name[today.month]} {today.year}'
     # writing the html code
     html_code_top = """
 {% extends "layout.html" %}
 
 {% block content %}"""
     html_code_core = f"""
-<script type="text/javascript" src="/static/js/current-albums-music.js" data_file="/data/archive/music_current_{date_string}.json"></script>
-<script type="text/javascript" src="/static/js/current-tracks-music.js" data_file0="/data/archive/music_current_songs_{date_string}.json"></script>
+<script type="text/javascript" src="/static/js/current-albums-music.js" data_file="/data/archive/{date_string}/music_current.json"></script>
+<script type="text/javascript" src="/static/js/current-tracks-music.js" data_file0="/data/archive/{date_string}/music_current_songs.json"></script>
+
+<div class="title-music">
+    <p class="section__subtitle section__subtitle--books">{date_range_string}</p>
+</div>
 
 <section class="my-albums" id="albums">
 </section>
@@ -101,8 +129,12 @@ def load_{file_name}():
     html_file = open(f'templates/archive/music/{date_string}.html', 'w')
     html_file.write(html_code)
 
+    f = open('data/archive/archive_num.txt', 'r')
+    archive_num = int(f.read())
+    f.close()
+
     render_new_template_function = f"""
-@app.route("/archive/music/{date_string}")
+@app.route("/archive/music/{archive_num}")
 def music_archive_{date_string}():
     return render_template("archive/music/{date_string}.html")
     """
@@ -110,8 +142,17 @@ def music_archive_{date_string}():
     with open('app.py', 'a') as app:
         app.write(render_new_template_function)
 
-    f = open('data_backup/last.txt', 'w')
+    f = open('data/archive/last.txt', 'w')
     f.write(date_string)
+    f.close()
+
+    f = open('data/archive/archive_num.txt', 'w')
+    archive_num += 1
+    f.write(str(archive_num))
+    f.close()
+
+    f = open('data/archive/last_date.txt', 'w')
+    f.write(date_string_string)
     f.close()
 
 def update_all_data():
@@ -125,4 +166,4 @@ def update_and_backup():
     update_all_data()
 
 if __name__ == "__main__":
-    update_and_backup()
+    backup_current_music_data()
