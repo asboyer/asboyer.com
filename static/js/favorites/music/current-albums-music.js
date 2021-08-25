3fd26d14ac57037973a9d0bc96516b64fa5265a1
boyer <asboyer@gmail.com>
2021-08-25 01:05:38 -0400

    shuffle for current songs

--- static/js/favorites/music/current-albums-music.js
+++ static/js/favorites/music/current-albums-music.js
@@ -1,15 +1,38 @@
 var data_file = document.currentScript.getAttribute('data_file');
+
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 console.log(data_file)
 $(document).ready(function(){
     var music_div = `
     <div class="albums">
     `
     $.getJSON(data_file, function(json) {
-        $.each(json, function(title, values){
-            console.log(title)
+
+        var albums = Object.values(json)
+        albums.sort((a,b) => (a.popularity < b.popularity) ? 1 : ((b.popularity < a.popularity) ? -1 : 0))
+        shuffle(albums)
+
+        $.each(albums, function(title, values){
             var tracklist = ""
             var styles = ""
-            if(title == "From Me To You" || title == "The College Dropout") {
+            if(values.name == "From Me To You" || values.name == "The College Dropout") {
                 tracklist += '-long'
             }
             // make an array of these albums for nowarp, same with small font
@@ -17,10 +40,10 @@ $(document).ready(function(){
             var album_div = 
             `<div class="album__container">
             <a href="https://open.spotify.com/album/${values.id}" class="album__item">
-                <img src="${values.image}" alt="${title}" class="portfolio__img">
+                <img src="${values.image}" alt="${values.name}" class="portfolio__img">
             <div class="album_overlay">
                 <div class="album-text">
-                    <p class="title" style="${styles}">${title.replace("(Deluxe)", "").replace("(Remastered)", "").replace("(Original Motion Picture Soundtrack)", "").replace("(Legacy Edition)", "").replace(" (Platinum VIP Edition)", "").replace(" (Original Television Series Soundtrack)", "").replace(" [Deluxe Edition]", "").split(" (Original")[0]}</p>
+                    <p class="title" style="${styles}">${values.name.replace("(Deluxe)", "").replace("(Remastered)", "").replace("(Original Motion Picture Soundtrack)", "").replace("(Legacy Edition)", "").replace(" (Platinum VIP Edition)", "").replace(" (Original Television Series Soundtrack)", "").replace(" [Deluxe Edition]", "").split(" (Original")[0]}</p>
                     <p class="artist">${values.artists}</p>
                     <div class="the-tracks">
                         <ul class="tracklist${tracklist}">
--- static/js/favorites/music/current-tracks-music.js
+++ static/js/favorites/music/current-tracks-music.js
@@ -1,11 +1,35 @@
 var data_file0 = document.currentScript.getAttribute('data_file0');
+
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 $(document).ready(function(){
     console.log(data_file0)
     var music_div = `
     <div class="songs">
     `
     $.getJSON(data_file0, function(json) {
-        $.each(json, function(title, values){
+
+        var songs = Object.values(json)
+        songs.sort((a,b) => (a.popularity < b.popularity) ? 1 : ((b.popularity < a.popularity) ? -1 : 0))
+        shuffle(songs)
+
+        $.each(songs, function(title, values){
             var style = ""
             // make an array of these albums for nowarp, same with small font
             // check length of title and then add special css
@@ -13,10 +37,10 @@ $(document).ready(function(){
             ` 
             <div class="song__container">
                 <a href="https://open.spotify.com/track/${values.id}" class="song__item">
-                    <img src="${values.image}" alt="${title}" class="portfolio__img">
+                    <img src="${values.image}" alt="${values.name}" class="portfolio__img">
                 <div class="song_overlay">
                     <div class="album-text">
-                        <p class="song-title" style="${style}">${title.split(' (feat')[0].split(' (with')[0].split(' (Main')[0]}</p>
+                        <p class="song-title" style="${style}">${values.name.split(' (feat')[0].split(' (with')[0].split(' (Main')[0]}</p>
                         <p class="song-artist">${values.artists}</p>
                     </div>
                 </div>
