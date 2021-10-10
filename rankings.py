966a746140ec769cb9fb5ea9ce8cc308b26ee8b6
boyer <asboyer@gmail.com>
2021-10-10 18:35:29 -0400

    rankins updates

--- rankings.py
+++ rankings.py
@@ -4,7 +4,13 @@ with open(f'./data/favorites/music/music_all_time.json', 'r') as json_file:
     all_time_data = json.load(json_file)
 
 for album in all_time_data:
-    score = (len(all_time_data[album]['top_tracks'])*1.25 + len(all_time_data[album]['good_tracks'])*.75)/(all_time_data[album]['total_tracks']*1.25)
+    if 'Kanye' in all_time_data[album]['artists']:
+        if all_time_data[album]['name'] == 'Graduation':
+            score = .95
+        else:
+            score = (len(all_time_data[album]['top_tracks'])*1.5 + len(all_time_data[album]['good_tracks'])*1.25)/(all_time_data[album]['total_tracks']*1.5)
+    else:
+        score = (len(all_time_data[album]['top_tracks'])*1.4 + len(all_time_data[album]['good_tracks']))/(all_time_data[album]['total_tracks']*1.4)
     all_time_data[album]['score'] = round(score * 10, 2)
 
 
