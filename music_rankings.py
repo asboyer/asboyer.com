9e61d161f86dce3c9039f8f0968b26e483e0d0ec
boyer <asboyer@gmail.com>
2021-10-14 23:49:45 -0400

    ranking updates

--- music_rankings.py
+++ music_rankings.py
@@ -5,7 +5,6 @@ with open(f'./data/favorites/music/music_all_time.json', 'r') as json_file:
 
 top = ['Kanye West', 
     'Playboi Carti', 
-    'Frank Ocean', 
     'Lil Uzi Vert', 
     'Michael Jackson', 
     'Post Malone', 
@@ -22,10 +21,13 @@ top = ['Kanye West',
     'Trippie Redd',
     'The Weekend',
     'Old Dominion',
-    'Tory Lanez']
+    'Tory Lanez',
+    'Miles Davis',
+    'Tim McGraw',
+    'Kendrick Lamar',
+    'Fleetwood Mac']
 
-top_a = ['Loose', 'So Much Fun', 'Flower Boy', 'Thats What They All Say', 'Sweet Action']
-
+top_a = ['?', 'Hoodie SZN', 'Loose', 'Confessions of a Dangerous Mind', 'So Much Fun', 'Flower Boy', 'Thats What They All Say', 'Sweet Action']
 for album in all_time_data:
     if all_time_data[album]['artists'] in top or all_time_data[album]['name'] in top_a:
         if all_time_data[album]['name'] == 'Graduation':
@@ -34,6 +36,8 @@ for album in all_time_data:
             score = .925
         else:
             score = (len(all_time_data[album]['top_tracks'])*1.5 + len(all_time_data[album]['good_tracks'])*1.25)/(all_time_data[album]['total_tracks']*1.5)
+    elif all_time_data[album]['artists'] == 'Frank Ocean':
+        score = (len(all_time_data[album]['top_tracks'])*2 + len(all_time_data[album]['good_tracks'])*1.87)/(all_time_data[album]['total_tracks']*2)
     else:
         score = (len(all_time_data[album]['top_tracks'])*1.4 + len(all_time_data[album]['good_tracks']))/(all_time_data[album]['total_tracks']*1.4)
     all_time_data[album]['score'] = round(score * 10, 2)
