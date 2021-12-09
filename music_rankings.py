3d907270a179668025d73ea7e4284ab49f8ae569
boyer <asboyer@gmail.com>
2021-12-09 17:22:38 -0500

    website updates

--- music_rankings.py
+++ music_rankings.py
@@ -28,15 +28,15 @@ top = ['Kanye West',
     'Fleetwood Mac'
     ]
 
-top_a = ['?', 'Hoodie SZN', 'Loose', 'Confessions of a Dangerous Mind', 'So Much Fun', 'Flower Boy', 'Thats What They All Say', 'Sweet Action']
+top_a = ['?', 'Boston', 'Hoodie SZN', 'Loose', 'Confessions of a Dangerous Mind', 'So Much Fun', 'Flower Boy', 'Thats What They All Say', 'Sweet Action']
 for album in all_time_data:
     if all_time_data[album]['artists'] in top or all_time_data[album]['name'] in top_a:
         if all_time_data[album]['name'] == 'Graduation':
             score = .98
         elif all_time_data[album]['name'] == "My Beautiful Dark Twisted Fantasy":
             score = .925
-        elif all_time_data[album]['name'] == 'Late Registration':
-            score = .979
+        # elif all_time_data[album]['name'] == 'Late Registration':
+        #     score = .979
         else:
             score = (len(all_time_data[album]['top_tracks'])*1.5 + len(all_time_data[album]['good_tracks'])*1.25)/(all_time_data[album]['total_tracks']*1.5)
     elif all_time_data[album]['artists'] == 'Frank Ocean':
