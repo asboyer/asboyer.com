7c2a6b3d3762c0f03346c6dc67844fa9eb76e3b5
boyer <asboyer@gmail.com>
2022-02-06 23:03:36 -0500

    blog post endgame

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
