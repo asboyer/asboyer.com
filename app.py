3ee68f55e841519132d7117df71af6d64765ecc6
boyer <asboyer@gmail.com>
2022-06-06 12:26:27 -0400

    music

--- app.py
+++ app.py
@@ -574,3 +574,21 @@ def load_music_current_songs_582022():
 def music_archive_582022():
     return render_template("archive/music/582022.html")
     
+#####################6/6/2022 music backup here#####################
+
+@app.route("/data/archive/music/662022/music_current.json")
+def load_music_current_662022():
+    f = open('data/archive/music/662022/music_current.json')
+    data = json.load(f)
+    return data
+
+@app.route("/data/archive/music/662022/music_current_songs.json")
+def load_music_current_songs_662022():
+    f = open('data/archive/music/662022/music_current_songs.json')
+    data = json.load(f)
+    return data
+
+@app.route("/archive/music/12")
+def music_archive_662022():
+    return render_template("archive/music/662022.html")
+    
