35f5473be7633b38aa9e44f6c4f26347855c4d53
Andrew Boyer <asboyer@gmail.com>
2023-07-25 17:06:42 -0400

    music experiment

--- music_rankings.py
+++ music_rankings.py
@@ -1,17 +1,44 @@
 import json
 
+top_track_weight = 1.25
+mid_track_weight = .75
+good_track_weight = 1
+
+# open all time file
 with open(f'./data/favorites/music/music_all_time.json', 'r') as json_file: 
     all_time_data = json.load(json_file)
+
 for album in all_time_data:
-    score = (len(all_time_data[album]['top_tracks'])*1.25 + len(all_time_data[album]['mid_tracks'])*.75 + len(all_time_data[album]['good_tracks']))/(all_time_data[album]['total_tracks']*1.25)
+    top_tracks = len(all_time_data[album]['top_tracks'])*top_track_weight
+    mid_tracks = len(all_time_data[album]['mid_tracks'])*mid_track_weight
+    good_tracks = len(all_time_data[album]['good_tracks'])*good_track_weight
+
+    total_tracks = all_time_data[album]['total_tracks']
+
+    score = (top_tracks + mid_tracks + good_tracks)/(total_tracks*top_track_weight)
+
     all_time_data[album]['algo_score'] = round(score * 10, 2)
-    all_time_data[album]['score'] = round((all_time_data[album]['algo_score'] + all_time_data[album]['asboyer_score']) / 2, 2)
+    score = all_time_data[album]['algo_score'] 
+
+    avg_score = round((score + all_time_data[album]['asboyer_score']) / 2, 2)
+
+    # if avg_score > score:
+    #   keep it unless score is below a 7
+
+    if avg_score < score and all_time_data[album]['asboyer_score'] > 7:
+        print(all_time_data[album]['name'])
+        avg_score = score
+
+    all_time_data[album]['score'] = avg_score
+
 with open(f'./data/favorites/music/music_all_time.json', 'w') as json_file: 
     json.dump(all_time_data, json_file, indent=4)
 
-
+# current favs file
 with open(f'./data/favorites/music/music_current.json', 'r') as json_file: 
     all_time_data = json.load(json_file)
+
+# if currently reviewing the album    
 for album in all_time_data:
     try:
         if len(all_time_data[album]['tracks']) > 0:
@@ -22,8 +49,26 @@ for album in all_time_data:
         go = True
 
     if go:
-        score = (len(all_time_data[album]['top_tracks'])*1.25 + len(all_time_data[album]['mid_tracks'])*.75 + len(all_time_data[album]['good_tracks']))/(all_time_data[album]['total_tracks']*1.25)
+        top_tracks = len(all_time_data[album]['top_tracks'])*top_track_weight
+        mid_tracks = len(all_time_data[album]['mid_tracks'])*mid_track_weight
+        good_tracks = len(all_time_data[album]['good_tracks'])*good_track_weight
+
+        total_tracks = all_time_data[album]['total_tracks']
+        score = (top_tracks + mid_tracks + good_tracks)/(total_tracks*top_track_weight)
+
         all_time_data[album]['algo_score'] = round(score * 10, 2)
-        all_time_data[album]['score'] = round((all_time_data[album]['algo_score'] + all_time_data[album]['asboyer_score']) / 2, 2)
+        score = all_time_data[album]['algo_score']
+
+
+        avg_score = round((score + all_time_data[album]['asboyer_score']) / 2, 2)
+
+        # if avg_score > score:
+        #   keep it unless score is below a 7
+
+        if avg_score > score and score > 7:
+            avg_score = score
+
+        all_time_data[album]['score'] = avg_score
+
 with open(f'./data/favorites/music/music_current.json', 'w') as json_file: 
     json.dump(all_time_data, json_file, indent=4)
